05d58d711015a156518aaf5732507fa8b9952114
Trey Dedman <treydedman@gmail.com>
2025-02-10 22:46:09 +0000

    completed typescript-modules

---
+++ typescript-modules/my-app/src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css';
+import { Header } from './Header.tsx';
+import { Footer } from './Footer.tsx';
+import { Body } from './Body.tsx';
+
+export function App() {
+  return (
+    <>
+      <Header />
+      <Body />
+      <Footer />
+    </>
+  );
+}
