041818387cd5309128eec7c2599fa317c8cb76e6
Trey Dedman <treydedman@gmail.com>
2025-02-14 20:03:22 +0000

    completed react-multiple-components

--- react-multiple-components/my-app/src/RotatingBanner.tsx
+++ react-multiple-components/my-app/src/RotatingBanner.tsx
@@ -1,36 +1,116 @@
+import { useState } from 'react';
+
 type Props = {
   items: string[];
 };
 
 export function RotatingBanner({ items }: Props) {
-  console.log(items);
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  // handleNextClick
+  function handleNextClick() {
+    if (currentIndex < items.length - 1) {
+      setCurrentIndex(currentIndex + 1);
+    } else {
+      setCurrentIndex(0);
+    }
+  }
+
+  // handlePrevClick
+  function handlePrevClick() {
+    if (currentIndex > 0) {
+      setCurrentIndex(currentIndex - 1);
+    } else {
+      setCurrentIndex(items.length - 1);
+    }
+  }
+
+  // handleSelect
+  function handleSelect(index: number) {
+    setCurrentIndex(index);
+  }
 
   return (
     <>
-      <Banner item="Aardvark" />
-      <PrevButton />
-      <Indicators count={items.length} />
-      <NextButton />
+      <Banner item={items[currentIndex]} />
+      <PrevButton onHandlePrevClick={handlePrevClick} />
+      <Indicators
+        count={items.length}
+        current={currentIndex}
+        onSelect={handleSelect}
+      />
+      <NextButton onHandleNextClick={handleNextClick} />
     </>
   );
 }
 
-function Banner({ item }: { item: string }) {
+type BannerProps = {
+  item: string;
+};
+function Banner({ item }: BannerProps) {
   return <h1>{item}</h1>;
 }
 
-function NextButton() {
-  return <button>Next</button>;
+type NextButtonProps = {
+  onHandleNextClick: () => void;
+};
+function NextButton({ onHandleNextClick }: NextButtonProps) {
+  return (
+    <button
+      style={{
+        backgroundColor: 'white',
+        border: '4px solid #aaa',
+        margin: '1px',
+        color: 'black',
+        cursor: 'pointer',
+      }}
+      onClick={onHandleNextClick}>
+      Next
+    </button>
+  );
 }
 
-function PrevButton() {
-  return <button>Prev</button>;
+type PrevButtonProps = {
+  onHandlePrevClick: () => void;
+};
+function PrevButton({ onHandlePrevClick }: PrevButtonProps) {
+  return (
+    <button
+      style={{
+        backgroundColor: 'white',
+        border: '4px solid #aaa',
+        margin: '1.5px',
+        color: 'black',
+        cursor: 'pointer',
+      }}
+      onClick={onHandlePrevClick}>
+      Prev
+    </button>
+  );
 }
 
-function Indicators({ count }: { count: number }) {
+type IndicatorProps = {
+  count: number;
+  current: number;
+  onSelect: (index: number) => void;
+};
+function Indicators({ count, current, onSelect }: IndicatorProps) {
   const buttons = [];
   for (let i = 0; i < count; i++) {
-    buttons.push(<button key={i}>{i}</button>);
+    buttons.push(
+      <button
+        key={i}
+        onClick={() => onSelect(i)}
+        style={{
+          backgroundColor: i === current ? 'lightblue' : 'white',
+          border: '4px solid #aaa',
+          margin: '1.5px',
+          color: 'black',
+          cursor: 'pointer',
+        }}>
+        {i}
+      </button>
+    );
   }
   return <div>{buttons}</div>;
 }
--- react-multiple-components/react-multiple-components-quiz-notes.md
+++ react-multiple-components/react-multiple-components-quiz-notes.md
@@ -6,9 +6,30 @@ Answer the following questions in the provided markdown file before turning in t
 
 - What are the phases in the React design process?
 
+start from a UI mockup
+-create static versions to view the components
+-verify it works as it should before implementing it dynamically
+
+determine the UI state that will be in the components
+-state can only flow from parent to child, as a prop
+-add calls to useState
+-verify the components work by hardcoding the value to implement changes
+
+determine the events that will modify the state
+-event handler props
+-the props are declared in the child components using the prefix "on"
+-parents pass the handler functions to the props and use the prefix "handle"
+-declare the props in the child components and call the props
+-implement the handler functions in the parent components and pass them to the props
+verify the components update correctly in response to events
+
 - How does data flow in React?
 
+one direction - parent to child
+
 - How can children components modify state in their parents?
+
+by receiving a prop from the parent, updates the state by using setState
 
 ## Notes
 
