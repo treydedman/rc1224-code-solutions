28e9ae3370bed316b48d8baad4af23f421f5bad1
Trey Dedman <treydedman@gmail.com>
2025-02-21 07:17:45 +0000

    ninety-eight percent done

---
+++ react-popup/my-app/src/Popup.tsx
@@ -0,0 +1,59 @@
+import { createPortal } from 'react-dom';
+import { ReactNode, useEffect } from 'react';
+import './Popup.css';
+
+type Props = {
+  open: boolean;
+  positionTo?: HTMLElement | null;
+  onClose: () => void;
+  children: ReactNode;
+  opacity: number;
+};
+
+export function Popup({
+  open,
+  positionTo,
+  onClose,
+  children,
+  opacity = 0.3,
+}: Props) {
+  if (!open) return null;
+
+  const r = positionTo?.getBoundingClientRect();
+  const top = r ? r.top + r.height : window.innerHeight / 2;
+  const left = r ? r.left + r.width / 2 : window.innerWidth / 2;
+
+  useEffect(() => {
+    function handleKeyDown(event: KeyboardEvent) {
+      if (event.key === 'Escape') {
+        onClose();
+      }
+    }
+    document.addEventListener('keydown', handleKeyDown);
+    return () => document.removeEventListener('keydown', handleKeyDown);
+  }, [onClose]);
+
+  return createPortal(
+    <>
+      <div
+        className="backdrop"
+        onClick={onClose}
+        style={{ background: `rgba(0, 0, 0, ${opacity})` }}></div>
+      <div
+        className="popup"
+        style={{
+          position: 'absolute',
+          top: `${top}px`,
+          left: `${left}px`,
+          transform: 'translateX(-50%)',
+          padding: '8px',
+          border: '1px solid black',
+          background: 'white',
+          zIndex: 1000,
+        }}>
+        {children}
+      </div>
+    </>,
+    document.body
+  );
+}
